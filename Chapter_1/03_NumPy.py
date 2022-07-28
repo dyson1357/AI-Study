cae26932048379e80be176b02bd518ec66bbd1bc
dyson1357 <dyson1357@naver.com>
2022-07-28 15:01:10 +0900

    파일 생성

--- Chapter_1/03_NumPy.py
+++ Chapter_1/03_NumPy.py
@@ -1,12 +1,10 @@
 import numpy as np
 
-array_int = np.array([1, 2, 3])
-array_float = array_int.astype('float64')
-print(array_float, array_float.dtype)
+array1d = np.arange(start=1, stop=10)
+array2d = array1d.reshape(3, 3)
+print(array2d)
 
-array_int1 = array_float.astype('int32')
-print(array_int1, array_int1.dtype)
-
-array_float1 = np.array([1.1, 2.1, 3.1])
-array_int2 = array_float1.astype('int32')
-print(array_int2, array_int2.dtype)
+print('(row=0, col=0) index 가리키는 값:', array2d[0, 0])
+print('(row=0, col=1) index 가리키는 값:', array2d[0, 1])
+print('(row=1, col=0) index 가리키는 값:', array2d[1, 0])
+print('(row=2, col=2) index 가리키는 값:', array2d[2, 2])
